8113a9bf1ae9663fb0e5797ef826ea27f03baf40
GLetiwa <graceletiwa@gmail.com>
2024-02-02 15:54:44 +0300

    new

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """handles configurations"""
 
-from flask import jsonify
+from flask import jsonify, request
 from api.v1.views import app_views
 from models import storage
 
@@ -9,18 +9,23 @@ from models import storage
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def get_status():
     """returns status of the api"""
-    return jsonify({"status": "OK"})
+    if request.method == 'GET':
+        return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats', methods=['GET'])
 def get_stats():
     """Retrieves the number of each object type"""
-    stats = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User"),
-    }
-    return jsonify(stats)
+    if request.method == 'GET':
+        response = {}
+        stats = {
+                "Amenity": "amenities",
+                "City": "cities",
+                "Place": "places",
+                "Review": "reviews",
+                "State": "states",
+                "User": "users"
+        }
+        for key, value in stats.items():
+            response[value] = storage.count()
+        return jsonify(response)
