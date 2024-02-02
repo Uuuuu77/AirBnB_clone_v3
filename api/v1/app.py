b483204aa5553462b62c57730187fd2f8248ef5a
GLetiwa <graceletiwa@gmail.com>
2024-02-02 15:13:39 +0300

    new

--- api/v1/app.py
+++ api/v1/app.py
@@ -23,6 +23,6 @@ def not_found(error):
 
 
 if __name__ == "__main__":
-    host = "0.0.0.0"
-    port = 5000
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(getenv('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, threaded=True)
