d4009c478c7cb3a1440b7cb9f6ff31bcde54169c
GLetiwa <graceletiwa@gmail.com>
2024-02-02 17:40:48 +0300

    new

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,5 +5,8 @@ from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-# Import views to register the routes
+from api.v1.views.index import *
+from api.v1.views.states import *
+
+
 from api.v1.views import *
