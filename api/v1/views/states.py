4ee23d7691f20a812ec0720c68a14d7bde07c801
John Njuguna <johnmnjuguna89@gmail.com>
2024-02-03 06:04:09 -0800

    State objects that handles all default RESTFul API actions

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -8,27 +8,25 @@ from api.v1.views import app_views
 
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def get_states():
     """Retrieves the list of all State objects"""
-    states = storage.all(State).values()
-    return jsonify([state.to_dict() for state in states])
+    if state_id is None:
+        states = storage.all(State).values()
+        return jsonify([state.to_dict() for state in states])
+    else:
+        state = storage.get(State, state_id)
+        if not state:
+            abort(404)
+        return jsonify(state.to_dict())
 
 
-@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
-def get_state(state_id):
-    """Retrieves a State object"""
-    state = storage.get(State, state_id)
-    if state is None:
-        abort(404)
-    return jsonify(state.to_dict())
-
-
-@app_views.route('/states/<state_id>',
-    methods=['DELETE'], strict_slashes=False)
+@app_views.route('/states/<state_id>', strict_slashes=False,
+                 methods=['DELETE'])
 def delete_state(state_id):
     """Deletes a State object"""
     state = storage.get(State, state_id)
-    if state is None:
+    if not state:
         abort(404)
     storage.delete(state)
     storage.save()
@@ -42,10 +40,9 @@ def create_state():
     if not data:
         abort(400, "Not a JSON")
     if 'name' not in data:
-        abort(400, "Missing name")
+        return jsonify({"error": "Missing name"}), 400
     new_state = State(**data)
-    storage.new(new_state)
-    storage.save()
+    new_state.save()
     return jsonify(new_state.to_dict()), 201
 
 
